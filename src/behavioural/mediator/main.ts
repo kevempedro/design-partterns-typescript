3f356aa3efd7ad8abf401ca59acc98842d314a77
Kevem Pedro <kplima@maisaedu.com.br>
2022-11-26 14:41:16 -0300

    Implementando o padrão Mediator

---
+++ src/behavioural/mediator/main.ts
@@ -0,0 +1,43 @@
+import { Buyer } from "./buyer";
+import { Mediator } from "./mediator";
+import { Seller } from "./seller";
+
+const mediator = new Mediator();
+
+const seller1 = new Seller();
+seller1.addProduct(
+    {
+        id: '1',
+        name: 'Camiseta',
+        price: 49.9
+    },
+    {
+        id: '2',
+        name: 'Caneta',
+        price: 9.9
+    }
+);
+
+const seller2 = new Seller();
+seller2.addProduct(
+    {
+        id: '3',
+        name: 'Carro',
+        price: 49000.9
+    },
+    {
+        id: '4',
+        name: 'Lápis',
+        price: 1.9
+    }
+);
+
+mediator.addSeller(seller1, seller2);
+
+const buyer = new Buyer(mediator);
+buyer.viewProducts();
+
+buyer.buy('2');
+buyer.buy('3');
+
+buyer.viewProducts();
---
+++ src/behavioural/mediator/seller-product.ts
@@ -0,0 +1,5 @@
+export type SellerProduct = {
+    id: string,
+    name: string,
+    price: number
+}
